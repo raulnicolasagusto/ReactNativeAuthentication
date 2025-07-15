2d6a7c4689659601a185b0e9a99e0fc29f5eb888
Raul Agusto <raulnicolasagusto@gmail.com>
2025-07-15 16:06:12 -0300

    iniciando

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 export default function App() {
+
+  const onPress1 = () => {
+      alert('Sign in button pressed');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Sign in</Text>
+
+      <TextInput autoCapitalize='none' placeholder="Email" style={styles.input} />
+      <TextInput placeholder="Password" style={styles.input} secureTextEntry />
+
+      <Button title="Sign in" onPress={onPress1} />
+      <Text>Don't have an account? Sign up</Text>
+
+      <Pressable style={styles.button} onPress={() => { console.log('Iniciar sesion pressed'); }}>
+        <Text style={styles.buttonText}>Iniciar sesion</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +29,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+  input: {
+    borderWidth: 1,
+    width: '95%',
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#f0f0f0',
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+
+  button: {
+    backgroundColor: '#007bff',
+    padding: 15,
+    borderRadius: 5,
+    width: '95%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
   },
 });
