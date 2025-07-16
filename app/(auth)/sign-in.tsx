b3e635f94cf5cadb82e93d61936715ec1884062e
Raul Agusto <raulnicolasagusto@gmail.com>
2025-07-16 18:55:46 -0300

    paginas sign in y sign out

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
+import CustomInput from '@/components/CustomInput';
+import CustomButton from '@/components/CustomButton';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link } from 'expo-router';
+
+// Validaciones con zod
+const signInSchema = z.object({
+  email: z.string({ message: 'Email is required' }).email('Invalid email address'),
+  password: z.string({ message: 'Password is required' }).min(8, 'Password must be at least 8 characters long'),
+});
+
+type SignInField = z.infer<typeof signInSchema>;
+
+export default function SignInScreen() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInField>({
+    resolver: zodResolver(signInSchema),
+  });
+
+  const onSignIn = (data: SignInField) => {
+    console.log('Sign in: ', data);
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Text style={styles.title}>Sign in</Text>
+
+      <View style={styles.form}>
+        <CustomInput<SignInField>
+          control={control}
+          name="email"
+          textContentType="emailAddress"
+          placeholder="Email"
+          autoComplete="email"
+          autoFocus
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        <CustomInput<SignInField>
+          control={control}
+          name="password"
+          secureTextEntry
+          placeholder="Password"
+          textContentType="password"
+          keyboardType="default"
+          autoCapitalize="none"
+        />
+      </View>
+      <CustomButton text="Iniciar sesion" onPress={handleSubmit(onSignIn)} />
+      <Link style={styles.link} href="/(auth)/sign-up">Don't have an account? Sign up</Link>
+
+      <StatusBar style="auto" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  form: {
+    width: '100%',
+    paddingHorizontal: 5,
+    gap: 5,
+  },
+    link: {
+        marginTop: 20,
+        fontSize: 18,
+        color: '#007BFF',
+    },
+});
---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,87 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
+import CustomInput from '@/components/CustomInput';
+import CustomButton from '@/components/CustomButton';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link } from 'expo-router';
+
+// Validaciones con zod
+const signUpSchema = z.object({
+  email: z.string({ message: 'Email is required' }).email('Invalid email address'),
+  password: z.string({ message: 'Password is required' }).min(8, 'Password must be at least 8 characters long'),
+});
+
+type SignUpField = z.infer<typeof signUpSchema>;
+
+export default function SignUpScreen() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpField>({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  const onSignUp = (data: SignUpField) => {
+    console.log('Sign up: ', data);
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Text style={styles.title}>Sign Up / Create an Account</Text>
+
+      <View style={styles.form}>
+        <CustomInput<SignUpField>
+          control={control}
+          name="email"
+          textContentType="emailAddress"
+          placeholder="Email"
+          autoComplete="email"
+          autoFocus
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        <CustomInput<SignUpField>
+          control={control}
+          name="password"
+          secureTextEntry
+          placeholder="Password"
+          textContentType="password"
+          keyboardType="default"
+          autoCapitalize="none"
+        />
+      </View>
+      <CustomButton text="Registrate" onPress={handleSubmit(onSignUp)} />
+      <Link style={styles.link} href="/(auth)/sign-in">Already have an account? Sign in</Link>
+
+      <StatusBar style="auto" />
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  form: {
+    width: '100%',
+    paddingHorizontal: 5,
+    gap: 5,
+  },
+  link: {
+    marginTop: 20,
+    fontSize: 18,
+    color: '#007BFF',
+  },
+});
