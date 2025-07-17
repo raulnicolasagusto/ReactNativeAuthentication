0f336d6f21f6a95e2c59910483cc93469a5993bb
Raul Agusto <raulnicolasagusto@gmail.com>
2025-07-17 15:17:32 -0300

    antalla de carga para evitar que se renderice el contenido antes de que se haya verificado la autenticación

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
-import { createContext, PropsWithChildren, useContext } from "react";
+import { createContext, PropsWithChildren, useContext, useEffect } from "react";
 import { useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 
 const AuthContext = createContext({
@@ -9,7 +10,16 @@ const AuthContext = createContext({
 });
 
 export const AuthProvider = ({  children }: PropsWithChildren) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | undefined>(undefined);
+
+    useEffect(() => {
+      const checkAuthentication = async () => {
+        // Simulate an authentication check (e.g., API call)
+         await new Promise((resolve) => setTimeout(resolve, 1000));
+        setIsAuthenticated(true);
+      };
+      checkAuthentication();
+    }, []);
 
     const signIn = () => {
         setIsAuthenticated(true);
@@ -19,11 +29,22 @@ export const AuthProvider = ({  children }: PropsWithChildren) => {
         setIsAuthenticated(false);
     }
 
-  return (
-    <AuthContext.Provider value={{isAuthenticated, signIn, signOut}}>
-      {children}
-    </AuthContext.Provider>
-  );
+
+    //pantalla de carga para evitar que se renderice el contenido antes de que se haya verificado la autenticación
+    //tener en cuenta que el useState debe ser de tipo boolean o undefined para poder mostrar el ActivityIndicator
+    if (isAuthenticated === undefined) {
+        return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator />
+        </View>
+        );
+    }
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, signIn, signOut }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
 export const useAuth = () => useContext(AuthContext)
