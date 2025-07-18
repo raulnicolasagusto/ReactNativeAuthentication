43b77c5123724f2f48d13f187a62718c2d8efd16
Raul Agusto <raulnicolasagusto@gmail.com>
2025-07-18 17:44:22 -0300

    saludamos al usuario en la pagina protected

--- app/(protected)/index.tsx
+++ app/(protected)/index.tsx
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
 
 export default function HomePage() {
+  const { user } = useUser();
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Home Page</Text>
       <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Only logged users can see this</Text>
+      {user && (
+        <Text style={{ fontSize: 20, marginTop: 20 }}>
+          Hola, {user.firstName || user.username || user.emailAddress}!
+        </Text>
+      )}
     </View>
   )
 }
